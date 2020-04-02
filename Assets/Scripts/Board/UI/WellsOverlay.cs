4a9a5c51a3d7606ca9a2caa1c72f4dbdc1f9d02b
Jonathan <jonathancolacocarr@gmail.com>
2020-04-02 16:24:27 -0400

    Added various icons for drinking, runestones, and potions. Started working on a Wells Overlay - triggered by the show wells button. Added a wells overlay script

---
+++ Assets/Scripts/Board/UI/WellsOverlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellsOverlay : MonoBehaviour
+{
+
+    public GameObject WellsPanel;
+
+    public void OpenPanel()
+    {
+        if(WellsPanel != null)
+        {
+            bool isActive = WellsPanel.activeSelf;
+            Debug.Log("Panel visibility:");
+            Debug.Log(isActive);
+            WellsPanel.SetActive(!isActive);
+        }
+    }
+}
