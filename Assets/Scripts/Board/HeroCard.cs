ed655a1a6f86aceb657b2adace111c617c5ce374
Gautam Mohanlal <gautam.mohanlal@mail.mcgill.ca>
2020-04-03 17:36:52 -0400

    Fixed Issues with Inventory

--- Assets/Scripts/Board/HeroCard.cs
+++ Assets/Scripts/Board/HeroCard.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryUI : MonoBehaviour
+public class HeroCard : MonoBehaviour
 {
-    // Reference to GameManager
-    private GameManager GameManager;
-
-    // References to children components
-   
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Board/UI/InventoryUIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIManager : MonoBehaviour//, Observer
+{
+    // Reference to GameManager
+    private GameManager GameManager;
+
+    // References to children components
+    GameObject Bow;
+    GameObject Brew;
+    GameObject Falcon;
+    GameObject Telescope;
+    GameObject Wineskin;
+    GameObject Shield;
+
+    public void Initialize()
+    {
+        // Initialize reference to GameManager
+       // GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        // Register as an observer of GameManager
+        //GameManager.Attach(this);
+
+        // Initialize references to children components
+        //
+
+        /// <summary>
+        /// Once the Hero icon is pressed, the HeroCard is activated. 
+        ///     - inventory manager checks and activates every item in the Hero's inventory 
+        ///     
+        ///     we need access to the hero class 
+        /// </summary>
+
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
