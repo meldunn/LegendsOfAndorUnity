1f4aca8475d72b9024c6f5aa7371d6044446a96a
Gautam Mohanlal <gautam.mohanlal@mail.mcgill.ca>
2020-04-05 14:12:30 -0400

    display HeroCard

---
+++ Assets/Scripts/Board/UI/HeroCardUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCardUI : MonoBehaviour
+{
+
+    GameObject WarriorHeroCard;
+    GameObject ArcherHeroCard;
+    GameObject DwarfHeroCard;
+    GameObject WizardHeroCard;
+
+    public void Initialize()
+    {
+
+        GameObject WarriorHeroCard = GameObject.Find("WarriorHeroCard");
+        GameObject ArcherHeroCard = GameObject.Find("ArcherHeroCard");
+        GameObject DwarfHeroCard = GameObject.Find("DwarfHeroCard");
+        GameObject WizardHeroCard = GameObject.Find("WizardHeroCard");
+
+    }
+
+    public void displayWarriorHeroCard()
+    {
+        if (WarriorHeroCard.activeSelf){
+            WarriorHeroCard.SetActive(false);
+        } else{
+            WarriorHeroCard.SetActive(true);
+        }   
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
