8727d9386c8feec14ac1ce223cb05688549ab722
kondvit <kondvit@users.noreply.github.com>
2020-04-08 03:05:47 -0400

    +hero selection menu (work in progress)

---
+++ Assets/Scripts/Networks/UI/HeroSelectionManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+//Autor: Vitaly
+
+
+
+public class HeroSelectionManager : MonoBehaviourPun
+{
+
+    [SerializeField]
+    private PlayerSelector player_0;
+    [SerializeField]
+    private PlayerSelector player_1;
+    [SerializeField]
+    private PlayerSelector player_2;
+    [SerializeField]
+    private PlayerSelector player_3;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_0.selectorOwner = PhotonNetwork.CurrentRoom.Players[0];
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Networks/UI/PlayerSelector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+
+//Autor: Vitaly
+
+public class PlayerSelector : MonoBehaviourPun
+{
+    [SerializeField]
+    public GameObject readyToken { get; private set; }
+    [SerializeField]
+    public Button next { get; private set; }
+    [SerializeField]
+    public Button prev { get; private set; }
+    [SerializeField]
+    public Image hero { get; private set; }
+
+    [Header("HeroSprites")]
+    [SerializeField]
+    private Sprite[] heroSprites;
+
+    [HideInInspector]
+    public Photon.Realtime.Player selectorOwner;
+
+
+
+
+
+
+    public bool isReady { get; private set; }
+
+    private void Start()
+    {
+       
+    }
+}
