6626e9c8b5fab0d70edd9c83f1a7e412ee43dd45
kondvit <kondvit@users.noreply.github.com>
2020-04-08 06:44:27 -0400

    +individual player selector, based on player count

--- Assets/Scripts/Networks/UI/HeroSelectionManager.cs
+++ Assets/Scripts/Networks/UI/HeroSelectionManager.cs
@@ -10,31 +10,55 @@ public class HeroSelectionManager : MonoBehaviourPun
 {
 
     [SerializeField]
-    private PlayerSelector player_0;
+    private Transform[] heroSelectorPositions;
     [SerializeField]
-    private PlayerSelector player_1;
+    private Transform parentCanvas;
+    private bool[] slotStatus = new bool[4]; //true for occupied, false for not occupied
+
     [SerializeField]
-    private PlayerSelector player_2;
-    [SerializeField]
-    private PlayerSelector player_3;
-
-
-
-
-
-
+    private GameObject selectorPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        player_0.selectorOwner = PhotonNetwork.CurrentRoom.Players[0];
+        //var playerID = PhotonNetwork.PlayerList;
+        print("we got here");
+        photonView.RPC("InstantiateSelector", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber - 1);
+
+        //print(id);
+        //InstantiateFlame();
+
+        //heroSelector.transform.SetParent(parentCanvas);
+        //heroSelector.transform.localScale = Vector3.one;
+
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+        //    for (int i = 0; i < slotStatus.Length; i++)
+        //    {
+        //        //if not occupied
+        //        if (!slotStatus[i])
+        //        {
+
+        //            heroSelector.GetComponent<PhotonView>().TransferOwnership
+        //            heroSelector.transform.SetParent(parentCanvas);
+        //            heroSelector.transform.localScale = Vector3.one;
+        //            slotStatus[i] = true; //set as occupied
+        //        }
+        //    }
+        //    PhotonNetwork.LocalPlayer.ActorNumber
+        //}
+
+
+        //player_0.selectorOwner = PhotonNetwork.CurrentRoom.Players[0];
 
 
     }
 
-    // Update is called once per frame
-    void Update()
+    [PunRPC] 
+    void InstantiateSelector(int id)
     {
-        
+        print("we got here2");
+        GameObject heroSelector = Instantiate(selectorPrefab, heroSelectorPositions[id].position, heroSelectorPositions[id].rotation, heroSelectorPositions[id]);
+        heroSelector.GetPhotonView().TransferOwnership(PhotonNetwork.LocalPlayer.ActorNumber);
     }
 }
--- Assets/Scripts/Networks/UI/PlayerSelector.cs
+++ Assets/Scripts/Networks/UI/PlayerSelector.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 
 //Autor: Vitaly
 
-public class PlayerSelector : MonoBehaviourPun
+public class PlayerSelector : MonoBehaviourPun, IPunOwnershipCallbacks
 {
     [SerializeField]
-    public GameObject readyToken { get; private set; }
+    private GameObject readyToken;
     [SerializeField]
-    public Button next { get; private set; }
+    private Button next;
     [SerializeField]
-    public Button prev { get; private set; }
+    private Button prev;
     [SerializeField]
-    public Image hero { get; private set; }
+    private Image hero;
 
     [Header("HeroSprites")]
     [SerializeField]
     private Sprite[] heroSprites;
-
-    [HideInInspector]
-    public Photon.Realtime.Player selectorOwner;
-
 
 
 
@@ -30,8 +27,26 @@ public class PlayerSelector : MonoBehaviourPun
 
     public bool isReady { get; private set; }
 
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        //if (targetView.IsMine)
+        //{
+        //    next.gameObject.SetActive(true);
+        //    prev.gameObject.SetActive(true);
+        //}
+    }
+
     private void Start()
     {
-       
+        if (photonView.IsMine)
+        {
+            next.gameObject.SetActive(true);
+            prev.gameObject.SetActive(true);
+        }
     }
 }
