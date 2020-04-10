55642bc0b03c50b2c17742aac37715088b37aad3
meldunn <46253456+meldunn@users.noreply.github.com>
2020-04-10 14:41:20 -0400

    fixed error in fog manager

--- Assets/Scripts/Board/FogManager.cs
+++ Assets/Scripts/Board/FogManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FogType { EventCard, Strength, Willpower, Gold, Wineskin, Gor };
+
 public class FogManager : MonoBehaviour
 {
 
-    public enum FogType = {EventCard, Strength, Willpower, Gold, Wineskin, Gor }
+   // public enum FogType = {EventCard, Strength, Willpower, Gold, Wineskin, Gor };
     
     // Start is called before the first frame update
     void Start()
