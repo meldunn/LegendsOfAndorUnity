9276f897703ab3da892f348864178defb53d2059
Stacey Beard <staceybeard10@gmail.com>
2020-04-11 22:34:29 -0400

    Add spinner (loading circle) prefab
    - Added a spinner prefab with a script that makes it spin.

---
+++ Assets/Scripts/Board/UI/Spinner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    RectTransform Transform;
+
+    [SerializeField]
+    [Tooltip("A larger number means a slower spin.")]
+    private int SpinSpeed;         
+
+    int SpinIncrement = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform Transform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpinIncrement < SpinSpeed) SpinIncrement += 1;
+        else
+        {
+            this.transform.Rotate(new Vector3(0, 0, -45));
+            SpinIncrement = 0;
+        }
+    }
+}
