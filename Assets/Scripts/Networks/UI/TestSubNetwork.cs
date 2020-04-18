79ee689c3bdcae9a8d0f3fe390372eb808353a9b
Stacey Beard <staceybeard10@gmail.com>
2020-04-17 22:34:56 -0400

    Add a networking test panel
    - Added a simple panel to test out and get used to using networking
      functionality.
    - The panel can be used by setting it to enabled (Canvases >
      MainCanvas > TestNetwork), building the game, and running multiple
      instances.

---
+++ Assets/Scripts/Networks/UI/TestSubNetwork.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSubNetwork
+{
+    string MyPhrase = "Default";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPhrase(string Text)
+    {
+        MyPhrase = Text;
+    }
+
+    public string GetPhrase()
+    {
+        return MyPhrase;
+    }
+}
