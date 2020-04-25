019f18e0ceef2cea4762f48247d2de519ae85fa7
Gautam Mohanlal <gautam.mohanlal@mail.mcgill.ca>
2020-04-25 13:50:21 -0400

    started ChatBehaviour

---
+++ Assets/Scripts/Board/ChatBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChatBehaviour
+{
+
+    [SerializeField]
+    GameObject ChatUI = null;
+    [SerializeField]
+    TMP_Text ChatMessages = null;
+    [SerializeField]
+    TMP_InputField InputMessages = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
