839785665a81b256ff52428ed3c7ee476d31d142
meldunn <46253456+meldunn@users.noreply.github.com>
2020-04-28 00:39:03 -0400

    pathbutton shows when click WPbutton

--- Assets/Scripts/Board/UI/WPButtonMoveUI.cs
+++ Assets/Scripts/Board/UI/WPButtonMoveUI.cs
@@ -15,8 +15,8 @@ public class WPButtonMoveUI : MonoBehaviour
                         19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
                         51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 81, 82, 84 };
 
-    public List<GameObject> WPButton = new List<GameObject>(73);
-    public List<GameObject> PathButton = new List<GameObject>(10);
+    public static List<GameObject> WPButton = new List<GameObject>(73);
+    public static List<GameObject> PathButton = new List<GameObject>(10);
 
     public void Initialize(GameManager GameManager)
     {
@@ -24,14 +24,6 @@ public class WPButtonMoveUI : MonoBehaviour
 
         //GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         GM = GameManager;
-
-        //Debug.Log("is gamemananger null? " + GameManager == null);
-        Debug.Log("is gamemananger null? ");
-
-        Debug.Log("------------" + (GM == null));
-
-        Debug.Log(GM);
-        Debug.Log(GM.GetCurrentTurnHero());
 
 
         //initialize WPmovebuttons
@@ -82,6 +74,7 @@ public class WPButtonMoveUI : MonoBehaviour
                 Visibility(PathButton[i], false);
             }
         }
+        Debug.Log("pathbutton count is " + PathButton.Count);
 
     }
 
@@ -149,17 +142,9 @@ public class WPButtonMoveUI : MonoBehaviour
     {
         Debug.Log("clicked");
         Debug.Log(" clicked is " + this.gameObject.name);
-
-
-        
-        // Debug.Log(GameManager);
-        // Debug.Log(GameManager.GetCurrentTurnHero().GetWaypoint().GetWaypointNum());
         Debug.Log(GM);
 
-        // Hero currHero = GameManager.GetCurrentTurnHero();
-
         Hero currHero = GM.GetCurrentTurnHero();
-
 
         //add to path
         //get path index
@@ -170,6 +155,40 @@ public class WPButtonMoveUI : MonoBehaviour
             {
                 currHero.path[index] = nameToPosInt(this.gameObject.name);
                 Debug.Log("hero path index is " + index + "wp number is " + currHero.path[index]);
+
+                int i = currHero.path[index];
+                int j;
+                j = i;
+                //check if special index
+                if (i == 81)
+                {
+                    j = 71;
+                }
+                else if (i == 82)
+                {
+                    j = 72;
+                }
+                else if (i == 84)
+                {
+                    j = 73;
+                }
+                string WaypointName = "";
+                WaypointName = "Waypoint (" + Location[j]+ ")";
+                Debug.Log("wp name is " + WaypointName);
+                Debug.Log("index is " + index);
+                Debug.Log(index);
+
+
+                Debug.Log("pathbutton size is " + PathButton.Count);
+                Debug.Log("check 0 pathbutton index is " + PathButton[0]);
+                Debug.Log("pathbutton index is " + PathButton[index]);
+                Waypoint waypoint = GameObject.Find(WaypointName).GetComponent<Waypoint>();
+                PathButton[index].transform.position = waypoint.transform.position;
+                Visibility(PathButton[index], true);
+                Debug.Log("here");
+                HideWPButtons();
+                Debug.Log("here1");
+
                 break;
             }
             else
@@ -204,6 +223,14 @@ public class WPButtonMoveUI : MonoBehaviour
                 }
                 Visibility(WPButton[j], true); //make WP button visible
             }
+        }
+    }
+
+    public void HideWPButtons()
+    {
+        for (int i = 0; i < 74; i++)
+        {
+            WPButton[i].SetActive(false); //hide
         }
     }
 
