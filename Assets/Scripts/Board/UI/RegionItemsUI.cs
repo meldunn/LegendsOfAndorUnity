4e0863371f092aef2fd0a899e62cb043346413e6
Gautam Mohanlal <gautam.mohanlal@mail.mcgill.ca>
2020-04-29 16:29:02 -0400

    ItemPanel script

---
+++ Assets/Scripts/Board/UI/RegionItemsUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionItemsUI : MonoBehaviour
+{
+
+    [SerializeField] GameObject Bow;
+    [SerializeField] GameObject Brew;
+    [SerializeField] GameObject BlueRune;
+    [SerializeField] GameObject Falcon;
+    [SerializeField] GameObject GreenRune;
+    [SerializeField] GameObject Helm;
+    [SerializeField] GameObject Shield;
+    [SerializeField] GameObject Telescope;
+    [SerializeField] GameObject Wineskin;
+    [SerializeField] GameObject YellowRune;
+    [SerializeField] GameObject Medicinalherb;
+
+
+
+
+
+}
