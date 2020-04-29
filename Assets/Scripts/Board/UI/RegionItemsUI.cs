68d58c0bac72baccefdd1425ae630259a19f1fe2
Gautam Mohanlal <gautam.mohanlal@mail.mcgill.ca>
2020-04-29 17:25:07 -0400

    RegionItems test

--- Assets/Scripts/Board/UI/RegionItemsUI.cs
+++ Assets/Scripts/Board/UI/RegionItemsUI.cs
@@ -17,7 +17,29 @@ public class RegionItemsUI : MonoBehaviour
     [SerializeField] GameObject YellowRune;
     [SerializeField] GameObject Medicinalherb;
 
+    public void Initialize()
+    {
+       
 
+    }
+
+
+    public void showItems(Dictionary<ItemType, int> Items)
+    {
+        if (Items[ItemType.Bow] > 0) Bow.SetActive(true);
+        if (Items[ItemType.Witchbrew] > 0) Brew.SetActive(true);
+        if (Items[ItemType.BlueRuneStone] > 0) BlueRune.SetActive(true);
+        if (Items[ItemType.Falcon] > 0) Falcon.SetActive(true);
+        if (Items[ItemType.GreenRuneStone] > 0) GreenRune.SetActive(true);
+        if (Items[ItemType.Helm] > 0) Helm.SetActive(true);
+        if (Items[ItemType.Shield] > 0) Shield.SetActive(true);
+        if (Items[ItemType.Telescope] > 0) Telescope.SetActive(true);
+        if (Items[ItemType.Wineskin] > 0) Wineskin.SetActive(true);
+        if (Items[ItemType.Wineskin] > 0) Wineskin.SetActive(true);
+        if (Items[ItemType.YellowRuneStone] > 0) YellowRune.SetActive(true);
+        if (Items[ItemType.MedicinalHerb] > 0) Medicinalherb.SetActive(true);
+
+    }
 
 
 
