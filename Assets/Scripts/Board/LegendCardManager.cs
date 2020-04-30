e562c080287ff7646d517fc9e00a8ce0688a5256
jjiang99 <55453135+jjiang99@users.noreply.github.com>
2020-04-30 01:55:27 -0400

    fixed helpOverlay sizing

--- Assets/Scripts/Board/LegendCardManager.cs
+++ Assets/Scripts/Board/LegendCardManager.cs
@@ -61,33 +61,16 @@ public class LegendCardManager : MonoBehaviourPun
 
     public void activateLegendCard_A()
     {
-        Debug.Log("LEGEND CARD A");
-        //A1
-        //Skral stronghold - 50 + dice roll is the region num
-        //Farmer placed on 28
-        Waypoint waypoint28 = GameObject.Find("Waypoint (28)").GetComponent<Waypoint>();
-        waypoint28.dropOneFarmer();
-        //creatureManager.SpawnTowerSkral();
-
-        headerText.text = "LEGEND CARD C";
+        //Debug.Log("LEGEND CARD A");
+        headerText.text = "LEGEND CARD A";
         narratorCardText.text = "";
         infoPanel.SetActive(true);
         infoPanel.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
-
-
-        //C2
-        //Gors placed on 27, 31
-        creatureManager.Spawn(CreatureType.Gor, 27);
-        creatureManager.Spawn(CreatureType.Gor, 31);
-        //Skral placed on 29
-        creatureManager.Spawn(CreatureType.Skral, 29);
-        //Prince Thorald placed on 72
-        heroManager.InitializeHero(HeroType.PrinceThorald, 72);
     }
 
     public void activateLegendCard_C(int regionNumber)
     {
-        Debug.Log("LEGEND CARD C");
+        //Debug.Log("LEGEND CARD C");
         //C1
         //Skral stronghold - 50 + dice roll is the region num
         //Farmer placed on 28
@@ -116,7 +99,7 @@ public class LegendCardManager : MonoBehaviourPun
 
     public void activateLegendCard_G()
     {
-        Debug.Log("LEGEND CARD G");
+        //Debug.Log("LEGEND CARD G");
         //Remove Prince Thorald
         //Wardraks placed on 26, 27 (10 Strength, 7 Willpower, 2 Black dice)
         creatureManager.Spawn(CreatureType.Wardrak, 26);
